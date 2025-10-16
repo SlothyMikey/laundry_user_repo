0b938ba914366e63de8e9ef19657618b35373702
Mikey <kerbydalan076@gmail.com>
2025-10-16 14:14:30 +0800

    Initial Setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import '@/global.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-primary">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
