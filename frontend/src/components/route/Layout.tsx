d2901b4a44de3cb65e28bbdc1c17d68140823ba6
Dencell-Jay <dencelljay22@gmail.com>
2025-11-15 15:34:22 +0800

    Added Header

--- frontend/src/components/route/Layout.tsx
+++ frontend/src/components/route/Layout.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Sidebar from '../ui/sidebar';
+import Header from '../ui/header';
 
 export default function Layout() {
   return (
-    <div>
+    <div className='flex h-screen'>
       {/* Sidebar */}
-      <Sidebar />
+      <div className='flex h-screen'>
+        <Sidebar />
+      </div>
 
-      <main>
+      <main className='flex flex-1 flex-col overflow-auto'>
+        {/* Header */}
+        <Header title='Title Here' subtitle='Subtitle Here'/>
+
         {/* Main content goes here */}
       </main>
     </div>
