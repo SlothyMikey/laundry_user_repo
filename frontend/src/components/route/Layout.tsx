bd0d7ecfd705c9f210997fedcd28793bdb6c24d2
Dencell-Jay <dencelljay22@gmail.com>
2025-11-15 13:49:58 +0800

    Added Sidebar

--- frontend/src/components/route/Layout.tsx
+++ frontend/src/components/route/Layout.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import Sidebar from '../ui/sidebar';
 
 export default function Layout() {
-  return <div>Layout</div>;
+  return (
+    <div>
+      {/* Sidebar */}
+      <Sidebar />
+
+      <main>
+        {/* Main content goes here */}
+      </main>
+    </div>
+  )
 }
